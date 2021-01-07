4b654a800c405a7b9a21d084b116bad8983ce28e
karata-sc <ss78gmad@gmail.com>
2021-01-07 23:26:14 +0900

    Edited Files

--- mypkg/scripts/slang_pub.py
+++ mypkg/scripts/slang_pub.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python2                                                                              # -*- coding: utf-8 -*-            
+#!/usr/bin/env python2
 import rospy
 from std_msgs.msg import UInt16
 import sys 
--- mypkg/scripts/slang_sub.py
+++ mypkg/scripts/slang_sub.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python2
-# -*- coding: utf-8 -*-
 import rospy
 from std_msgs.msg import UInt16
 
